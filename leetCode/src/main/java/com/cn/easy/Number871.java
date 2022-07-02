d932e61b258bcb3493f03db4b725a253ac129b9e
Jossc <1723942375@qq.com>
2022-07-02 21:02:00 +0800

    1main1

---
+++ leetCode/src/main/java/com/cn/easy/Number871.java
@@ -0,0 +1,27 @@
+package com.cn.easy;
+
+/**
+ * author: 一方通行
+ */
+public class Number871 {
+
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int n = stations.length;
+        long[] dp = new long[n + 1];
+        dp[0] = startFuel;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (dp[i] >= stations[i][0]) {
+                    dp[j + 1] = Math.max(dp[j + 1], dp[j] + stations[i][1]);
+                }
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+            if (dp[i] >= target) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
